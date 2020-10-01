9cdc0b9566bc70701535428a7b9c15afcde67619
Nitin-yadav17 <nitinyadav70707@gmail.com>
2020-10-01 18:39:01 +0530

    Update array.java

--- array.java
+++ array.java
@@ -1,9 +1,9 @@
 class array {
     public static void main(String[] args) {
-     
+    try{
       // create an array
       int[] age = {12, 4, 5, 2, 5};
-   
+
       // access each array elements
       System.out.println("Accessing Elements of Array:");
       System.out.println("First Element: " + age[0]);
@@ -11,5 +11,6 @@ class array {
       System.out.println("Third Element: " + age[2]);
       System.out.println("Fourth Element: " + age[3]);
       System.out.println("Fifth Element: " + age[4]);
-    }
+    }catch(Exception e){};
+  }
    }
